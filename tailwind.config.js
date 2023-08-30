c5fe05578f9fa1d9a7be40a2d1b47714a4ee5403
Ahmad.Kahfi <Ahmad.kahfi@astralife.co.id>
2023-08-30 18:14:31 +0700

    20%

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "background-color": "var(--background-color)",
+      "primary-color": "var(--primary-color)",
+      "secondary-color": "var(--secondary-color)",
+      "third-color": "var(--third-color)",
+      "fourth-color": "var(--fourth-color)",
+    },
+  },
+  plugins: [],
+}
+
